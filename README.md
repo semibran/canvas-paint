2ac2389f8436f0840770594254e353338d30fb94
Brandon Semilla <semibran@gmail.com>
2017-05-08 17:22:25 -0700

    Clean up

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # canvas-draw
-> Drawing methods for Canvas API
+> Drawing methods for <canvas> API
 
-**Note:** This module is a work in progress. More methods will be added as necessary.
+**Note:** This module is a work in progress. More methods will be added/removed as deemed necessary.
 
 ```javascript
 const Draw = require('canvas-draw')
@@ -11,15 +11,14 @@ Draw(canvas)
   .clear(width, height)(x, y) // or clear whole canvas with `clear()`
   .rect(color, width, height)(x, y)
   .circle(color, radius)(x, y)
-  .arc(color, radius, 0, 90)(x, y) // accepts angles instead of radians
+  .arc(color, radius, 0, 2 * Math.PI)(x, y)
   .image(image, width, height)(x, y) // or omit width and height to use natural dimensions
-  .pixels(imageData) // or omit argument to get pixel data
 ```
 
-## Installation
+## install
 ```sh
 npm install semibran/canvas-draw
 ```
 
-## License
+## license
 MIT
--- index.js
+++ index.js
@@ -1,77 +1,54 @@
 module.exports = function Draw(canvas) {
 
 	var context = canvas.getContext('2d')
-	var methods =
-    { fill
-    , clear
-    , rect
-    , circle
-    , arc
-    , image
-    , pixels
-    }
+	var methods = { fill, clear, rect, circle, arc, image, pixels }
 
 	return methods
 
-  // fill(color)
 	function fill(color) {
-    rect(color, canvas.width, canvas.height)(0, 0)
+		rect(color, canvas.width, canvas.height)(0, 0)
 		return methods
 	}
 
-  // clear(width, height)(x, y)
 	function clear(width, height) {
-		if (!arguments.length)
-      return clear(canvas.width, canvas.height)(0, 0)
-    return function at(x, y) {
-  		context.clearRect(x, y, width, height)
-  		return methods
-    }
+		if (!arguments.length) {
+			return clear(canvas.width, canvas.height)(0, 0)
+		}
+		return function at(x, y) {
+			context.clearRect(x, y, width, height)
+			return methods
+		}
 	}
 
-  // rect(color, width, height)(x, y)
 	function rect(color, width, height) {
-    return function at(x, y) {
-      context.fillStyle = color
-  		context.fillRect(x, y, width, height)
-  		return methods
-    }
+		return function at(x, y) {
+			context.fillStyle = color
+			context.fillRect(x, y, width, height)
+			return methods
+		}
 	}
 
-  // circle(color, radius)(x, y)
-  function circle(color, radius) {
-    return arc(color, radius, 0, 360)
-  }
+	function circle(color, radius) {
+		return arc(color, radius, 0, 2 * Math.PI)
+	}
 
-  // arc(color, radius, startAngle, endAngle, anticlockwise)(x, y)
-  function arc(color, radius, startAngle, endAngle, anticlockwise) {
-    startAngle = (startAngle - 90) * Math.PI / 180
-    endAngle = (endAngle - 90) * Math.PI / 180
-    return function at(x, y) {
-      context.fillStyle = color
-      context.beginPath()
-      context.arc(x, y, radius, startAngle, endAngle, anticlockwise)
-      context.lineTo(x, y)
-      context.fill()
-    }
-  }
+	function arc(color, radius, start, end, anticlockwise) {
+		return function at(x, y) {
+			context.fillStyle = color
+			context.beginPath()
+			context.arc(x, y, radius, start, end, anticlockwise)
+			context.fill()
+		}
+	}
 
-  // image(image, width, height)(x, y)
 	function image(image, width, height) {
 		if (arguments.length === 1) {
 			width = image.width
 			height = image.height
 		}
-    return function at(x, y) {
-  		context.drawImage(image, x, y, width, height)
-  		return methods
-    }
+		return function at(x, y) {
+			context.drawImage(image, x, y, width, height)
+			return methods
+		}
 	}
-
-  function pixels(imageData) {
-    if (!imageData)
-      return context.getImageData(0, 0, canvas.width, canvas.height)
-    context.putImageData(imageData, 0, 0)
-    return methods
-  }
 }
